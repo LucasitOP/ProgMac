9a3d17d1891fc7dcae57dabf18f5c95447f9ce83
LucasitOP <lucas.timoc1@gmail.com>
2025-02-19 23:53:48 +0100

    Cuaderno ArrayBidimensionales 2 terminado

--- SegundaEvaluacion/ArrayBidimensionales/ArrayBidimensional2/ejercicio4.java
+++ SegundaEvaluacion/ArrayBidimensionales/ArrayBidimensional2/ejercicio4.java
@@ -1,4 +1,7 @@
 package ArrayBidimensional2;
+
+import java.util.Arrays;
+
 /*
  *  Dada una matriz cuadrada obtener cual es la suma de todos los elementos que
 componen las dos diagonales.
@@ -13,7 +16,23 @@ Probarlo con:
 public class ejercicio4 {
 
 	public static void main(String[] args) {
-
+		int [][]matriz={{1, 2, 3, 4, 5},
+				{ 6, 7, 8, 9,10},
+				{11,12,13,14,15},
+				{16,17,18, 19,20},
+				{21,22,23,24,25}};
+		int diagonal1=0;
+		int diagonal2=0;
+		
+		for (int i=0;i<matriz.length;i++) {
+		diagonal1 += matriz[i][i];	
+		}
+		for (int j=0;j<matriz.length;j++) {
+			diagonal2 += matriz[(matriz.length-1)-j][j];
+		}
+		System.out.println(Arrays.deepToString(matriz));
+		System.out.println("1ª Diagonal:"+diagonal1);
+		System.out.println("2ªDiagonal:"+diagonal2+"\n Suma total:"+(diagonal1+diagonal2));
 	}
 
 }
--- SegundaEvaluacion/ArrayBidimensionales/ArrayBidimensional2/ejercicio5.java
+++ SegundaEvaluacion/ArrayBidimensionales/ArrayBidimensional2/ejercicio5.java
@@ -1,10 +1,34 @@
 package ArrayBidimensional2;
 
-public class ejercicio5 {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ejercicio5 {		    public static void main(String[] args) {
+		        int filas = 4, columnas = 5;
+		        int[][] matriz = new int[filas][columnas];
 
-	}
+		        int sumaTotal = 0; 
 
-}
+		        for (int i = 0; i < filas - 1; i++) {
+		            int sumaFila = 0;
+		            for (int j = 0; j < columnas - 1; j++) {
+		                matriz[i][j] = (int) (Math.random() * (999 - 100 + 1) + 100);
+		                sumaFila += matriz[i][j]; 
+		                matriz[filas - 1][j] += matriz[i][j]; 
+		            }
+		            matriz[i][columnas - 1] = sumaFila; 
+		            sumaTotal += sumaFila;
+		        }
+
+		        for (int j = 0; j < columnas - 1; j++) {
+		            sumaTotal += matriz[filas - 1][j];
+		        }
+
+		        matriz[filas - 1][columnas - 1] = sumaTotal;
+
+		        for (int[] fila : matriz) {
+		            System.out.println(Arrays.toString(fila));
+		        }
+		    }
+		}
+
+
